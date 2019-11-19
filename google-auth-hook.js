49ffdf99711445921f0b6b6a291e5397b6f78ba7
Agon Lohaj <agonlohaj@odysseyprime.nl>
2019-11-19 17:48:05 +0100

    clientID from clientId

--- google-auth-hook.js
+++ google-auth-hook.js
@@ -26,7 +26,7 @@ const GoogleOAuth2Strategy = require('@passport-next/passport-google-oauth2')
 passport.use(
     new GoogleOAuth2Strategy(
         {
-            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: process.env.GOOGLE_CALLBACK_URL,
             scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
